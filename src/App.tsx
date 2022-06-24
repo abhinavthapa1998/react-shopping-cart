7aa0f74467d39e18b1a8a8e72a0cc59d55565abf
Abhinav Thapa <abhinav.thapa@writlabs.com>
2022-06-24 22:45:13 +0530

    First Commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,46 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import { useQuery } from "react-query";
+import { Drawer } from "@mui/material";
+import LinearProgress from "@mui/material";
+import Grid from "@mui/material";
+import { AddShoppingCart } from "@mui/icons-material";
+import Badge from "@mui/material";
+import { Wrapper } from "./App.styles";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+export type CartItemType = {
+  id: number;
+  category: string;
+  description: string;
+  image: string;
+  price: number;
+  title: string;
+  amount: number;
+};
+
+const getProducts = async (): Promise<CartItemType[]> => {
+  return await (await fetch("https://fakestoreapi.com/products")).json();
+};
+const App = () => {
+  const { data, isLoading, error } = useQuery<CartItemType[]>(
+    "products",
+    getProducts
   );
-}
+  const getTotalItems = () => {
+    return null;
+  };
+  const handleAddToCart = (clickedItem: CartItemType) => {
+    return null;
+  };
+  const handleRemoveFromCart = () => {
+    return null;
+  };
+  if (isLoading) {
+    return <LinearProgress />;
+  }
+  if (error) {
+    return <div>Something went wrong...</div>;
+  }
+  return <div className="App">Start</div>;
+};
 
 export default App;
